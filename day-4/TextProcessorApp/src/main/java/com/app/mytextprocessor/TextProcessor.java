fd4e224b1d90a8e69a06e3d68913328fea42cdd4
Ramanuj Das <ramanujds9@gmail.com>
2024-03-13 04:04:59 +0530

    Updated

---
+++ day-4/TextProcessorApp/src/main/java/com/app/mytextprocessor/TextProcessor.java
@@ -0,0 +1,48 @@
+package com.app.mytextprocessor;
+
+import java.util.function.Function;
+
+public class TextProcessor {
+
+    // Functionality: String repetition
+    public static String repeatString(String str, int count) {
+        // Todo: Implement string repetition using String::repeat(int count)
+        return null;
+    }
+
+    // Functionality: Checking if a string is blank
+    public static boolean isStringBlank(String str) {
+        // Todo: Implement checking if a string is blank using String::isBlank()
+        return false;
+    }
+
+    // Functionality: Extracting lines from a multi-line string
+    public static void extractLines(String multiLineStr) {
+        // Todo: Implement extracting lines from a multi-line string using String::lines()
+    }
+
+    // Functionality: String formatting
+    public static String formatString(String format, Object... args) {
+        // Todo: Implement string formatting using String::format(String format, Object... args)
+        return null;
+    }
+
+    // Functionality: String transformation
+    public static String transformString(String str, Function<? super String, ? extends String> f) {
+        // Todo: Implement string transformation using String::transform(Function<? super String, ? extends String> f)
+        return null;
+    }
+
+    // Functionality: String indentation
+    public static String indentString(String str, int n) {
+        // Todo: Implement string indentation using String::indent(int n)
+        return null;
+    }
+
+    // Todo: Add error handling for edge cases such as null inputs
+
+    // Todo: Add more advanced functionalities utilizing other features of the Java String API
+
+    // Todo: Expand the application to handle file I/O for processing text files
+}
+
